f83570b88a7ed41832028b905880886a672ca510
Jehon <1582670+jehon@users.noreply.github.com>
2021-11-16 12:12:03 +0100

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,19 +1,11 @@
 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 choco feature enable -n=allowGlobalConfirmation
 
-choco install git
-choco install docker-desktop
-choco install vcbuildtools
-choco install naps2
-choco install mobaxterm
-choco install hashtab
-choco install psutils
-choco install firefox
-choco install vscode
-choco install wsl2
-choco install google-drive-file-stream
-choco install jdk11 -params "static=false"
+choco install git docker-desktop vcbuildtools naps2 mobaxterm hashtab psutils firefox vscode wsl2 google-drive-file-stream
 
+# choco install jdk11 -params "static=false"
 # choco install freemind
 # choco install gradle
 # choco install nano
