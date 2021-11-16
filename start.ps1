4dbb26876b754cf79900adde5d532cf259a84943
Jehon <1582670+jehon@users.noreply.github.com>
2021-11-16 11:53:18 +0100

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,24 +1,3 @@
-﻿
-# $RootDir = Split-Path -parent $PSScriptRoot
-$RootDir = $PSScriptRoot
-
-# Write-Output $PSScriptRoot $RootDir
-# . $RootDir\lib\require-admin.ps1
-
-if (Enter-Admin) {
-    Write-Output "This script is restated as admin"
-    Exit 0
-}
-
-Get-ChildItem "$RootDir\etc" -Filter *.reg | Foreach-Object {
-    $F = "$RootDir\etc\$_"
-    Write-Output "Loading $F"
-    regedit /S "$F"
-}
-
-# explorer.exe shell:startup
-Write-Output "Installing startup scripts"
-Copy-Item -Recurse -Force -Path "$RootDir\startup\*" -Destination "C:\Users\jho\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 
 choco feature enable -n=allowGlobalConfirmation
 
