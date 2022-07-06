577bd5df9bf7846e1914f2b67780ce8144db83f3
Jean Honlet <jehon@users.noreply.github.com>
2022-07-06 16:53:57 +0200

    rework: reunit start and setup

--- start.ps1
+++ start.ps1
@@ -1,6 +1,20 @@
 
-# Get-Help Invoke-Expression
+#
+# We need to be admin to run this script
+# 
+if (Enter-Admin) {
+    Write-Output "This script is restated as admin"
+    Exit 0
+}
+# Write-Output $PSScriptRoot $RootDir
+# . $RootDir\lib\require-admin.ps1
 
+$RootDir = $PSScriptRoot
+
+#
+# Do we have choco installed?
+#
+Write-Output "Checking choco is installed..."
 $testchoco = powershell choco -v
 if (-not($testchoco)) {
   Write-Output "Installing choco"
@@ -8,15 +22,59 @@ if (-not($testchoco)) {
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
   iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
-
 choco feature enable -n=allowGlobalConfirmation
+Write-Output "Checking choco is installed done"
 
-choco install `
-  docker-desktop `
-  firefox `
-  git `
-  google-drive-file-stream `
-  greenshot `
+#
+# Loading some reg files
+#
+Write-Output "Loading some reg files..."
+Get-ChildItem "$RootDir\etc" -Filter *.reg | Foreach-Object {
+    $F = "$RootDir\etc\$_"
+    Write-Output "Loading $F"
+    regedit /S "$F"
+}
+Write-Output "Loading some reg files done"
+
+#
+# explorer.exe shell:startup
+#
+Write-Output "Installing startup scripts..."
+Copy-Item -Recurse -Force -Path "$RootDir\startup\*" -Destination "C:\Users\jho\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+Write-Output "Installing startup scripts done"
+
+Write-Output "Installing choco packages..."
+choco install hashtab
+choco install jdk11 -params "static=false"
+choco install mobaxterm
+choco install naps2
+choco install psutils
+choco install tortoisegit
+choco install vagrant
+choco install vcbuildtools
+choco install xmind
+
+# Non tested
+choco install clickshare-desktop
+choco install digikam
+choco install firefox
+choco install greenshot
+choco install logitech-options
+choco install plantronicshub
+choco install unifying
+choco install supertuxkart
+
+# choco install freemind
+
+Write-Output "Installing choco packages done"
+
+#
+# Configure some stuff
+#
+
+# For Vagrant
+# [Environment]::SetEnvironmentVariable("VAGRANT_DEFAULT_PROVIDER", "hyperv", "User")
+
   hashtab `
   logitech-options `
   mobaxterm `
@@ -32,12 +90,3 @@ choco install `
 
 wsl --set-default-version 2
 wslconfig /setdefault Ubuntu
-
-# Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
-
-# gradle jdk11 maven
-# vcbuildtools
-
-# choco install freemind
-# choco install nano
-# choco install xmind
