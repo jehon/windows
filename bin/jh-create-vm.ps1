567aeb407d4c5ef272d63d8040a3853b8cc2ea80
Jean Honlet <jehon@users.noreply.github.com>
2022-07-07 13:31:45 +0200

    feat(vm): create the vm
    but do not start it
    and not failsafe

--- bin/jh-create-vm.ps1
+++ bin/jh-create-vm.ps1
@@ -1,23 +1,38 @@
 
 # https://docs.microsoft.com/en-us/windows-server/virtualization/hyper-v/get-started/create-a-virtual-machine-in-hyper-v
+# https://docs.microsoft.com/en-us/virtualization/hyper-v-on-windows/quick-start/try-hyper-v-powershell
 
-$NetworkAdapterName = "vEthernet (J Internal 192.168.100.1)"
+# Install-WindowsFeature -Name Hyper-V -IncludeManagementTools -Restart
+
+$VMName = "dev"
+$VMRoot = "C:\users\jho\src\vm"
+$NetworkSwitchName = "J - 192.168.100.1"
 
 Write-Output "* Network adaptator"
+# Get-VMSwitch  * | Format-Table Name
 
-Write-Output "** Listing network adaptator"
-Get-NetAdapter | Select-Object -Property Name
-
-Write-Output "** Testing if required is present"
-if ( (Get-NetAdapter $NetworkAdapterName) ) {
+if ( (Get-VMSwitch $NetworkSwitchName) 2> $null) {
     Write-Output "[I] The network adapter already exists"
 } else {
     Write-Output "[I] Creating the network adapter"
-    New-VMSwitch -Name privateSwitch -SwitchType Private -Name $NetworkAdapterName -NetAdapterName $NetworkAdapterName
+    New-VMSwitch -Name $NetworkSwitchName -SwitchType Internal
 }
 
 Write-Output "* Creating the disk"
+New-VHD -Path $VMRoot\disk.vhdx -SizeBytes 60GB -Fixed
 
 Write-Output "* Creating the VM"
+New-VM -Name $VMName `
+    -Path $VMRoot\dev `
+    -MemoryStartupBytes 4096Mb `
+    -VHDPath $VMRoot\disk.vhdx `
+    -Switch $NetworkSwitchName `
 
-Write-Output "* Setup the VM"
+Set-VMMemory -VMName $VMName `
+    -DynamicMemoryEnabled 0
+#    -MaximumBytes <Int64>
+#    -MinimumBytes <Int64>
+
+Add-VMNetworkAdapter -VMName $VMName `
+    -SwitchName "Default Switch"
+
