159c89ef25d658e98e95e956263d15bd260e930a
Jean Honlet <jehon@users.noreply.github.com>
2022-07-11 10:28:29 +0200

    fix: add more git config

--- start-user.ps1
+++ start-user.ps1
@@ -1,3 +1,16 @@
 
+
+Write-Output "Configuring git..."
 git config --global user.name "Jean Honlet"
 git config --global user.email jehon@users.noreply.github.com
+
+git config --global push.default current
+# Push tags
+git config --global push.followtags true
+git config --global pull.rebase true
+git config --global rebase.autoStash true
+git config --global fetch.prune true
+git config --global fetch.writeCommitGraph true
+git config --global init.defaultBranch main
+Write-Output "Configuring git done"
+
