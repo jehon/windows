844be9f2b30d7be746f926ef63ec3d1a2d8c9b16
Jean Honlet <jehon@users.noreply.github.com>
2022-11-30 09:07:15 +0100

    style: add messages

--- bin/jh-d365-split-tickets.js
+++ bin/jh-d365-split-tickets.js
@@ -18,7 +18,7 @@ assert.match("FM-22", REGEX);
 assert.match("ticket FM-22", REGEX);
 assert.doesNotMatch("bonjour ceci est mon ticket", REGEX);
 
-process.stdout.write(`FILE: ${F_INPUT}\n`);
+process.stdout.write(`Reading ${F_INPUT}\n`);
 try {
     fs.statSync(F_INPUT);
 } catch (e) {
@@ -36,10 +36,10 @@ for(let i = 0; i< initialData.length; i++) {
     const line = initialData[i];
 
     // Add other lines according to comments:
-    const comment = line['Commentaire externe'];
+    const comment = line['Commentaire interne'] ?? line['Commentaire externe'];
     const tickets = [ ...(comment??"").matchAll(new RegExp(REGEX, 'g')) ].map(m => m[0]);
     if (tickets && tickets.length > 0) {
-        console.log(comment, " => ", tickets.join(', '), line);
+        console.log("Replacing ", comment, " => ", JSON.stringify(tickets));
         const n = tickets.length;
         // "Heures"
         for(let i = 0; i < n; i++ ) {
@@ -48,7 +48,6 @@ for(let i = 0; i< initialData.length; i++) {
                 splitted: true,
                 splitted_Heures: line.Heures / n,
                 splitted_Ticket: tickets[i]
-
             });
         }
     } else {
@@ -65,3 +64,5 @@ for(let i = 0; i< initialData.length; i++) {
 
 XLSX.utils.book_append_sheet(workbook, XLSX.utils.json_to_sheet(outputData), "splitted", true);
 XLSX.writeFile(workbook, F_OUTPUT);
+
+process.stdout.write(`Generated ${F_OUTPUT}`);
