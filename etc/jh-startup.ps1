38176cc11abbd4023f7d7fd8a125b28cc472d704
Jean Honlet <jehon@users.noreply.github.com>
2022-12-12 12:16:36 +0100

    fix: ssh for local host

--- etc/jh-startup.ps1
+++ etc/jh-startup.ps1
@@ -1,3 +1,18 @@
 
+$ErrorActionPreference = "Stop"
+
+# Get-WMIObject Win32_networkadapter | Select-Object Name, AdapterType, InterfaceIndex | Sort Name | Format-Table
+
+$idx=(Get-WMIObject Win32_networkadapter `
+    | Select-Object Name, AdapterType, InterfaceIndex `
+    | Where-Object -Property Name -eq 'Hyper-V Virtual Ethernet Adapter' `
+)[0].InterfaceIndex
+
+Write-Output "[I] IDX: $idx"
+
+$ssh_initial = Get-Content -path ~\.ssh\config -Raw
+$ssh_new = $ssh_initial -replace "HostName fe80::200:ff:fe00:1f%%.*", "HostName fe80::200:ff:fe00:1f%%$idx"
+$ssh_new | Set-Content -NoNewline -Path ~\.ssh\config
+
 # Initialize the ssh key (docker would otherwise cause problems)
 ssh -o StrictHostKeyChecking=accept-new root@dev echo "ok"
