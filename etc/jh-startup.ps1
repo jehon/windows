9453765b7ac05e79744b47763dec39c0adbcaf67
Jean Honlet <jehon@users.noreply.github.com>
2022-12-27 11:43:36 +0100

    refactor: jh-startup

--- etc/jh-startup.ps1
+++ etc/jh-startup.ps1
@@ -1,28 +1,30 @@
 
 $ErrorActionPreference = "Stop"
 
+Write-Output "* Getting hyperv adapter..."
 # Get-WMIObject Win32_networkadapter | Select-Object Name, AdapterType, InterfaceIndex | Sort Name | Format-Table
 
-Write-Output "* Adapting the dev config..."
 $idx=(Get-WMIObject Win32_networkadapter `
     | Select-Object Name, AdapterType, InterfaceIndex `
     | Where-Object -Property Name -eq 'Hyper-V Virtual Ethernet Adapter' `
 )[0].InterfaceIndex
-
 Write-Output "[I] IDX: $idx"
+Write-Output "* Getting hyperv adapter done"
 
+Write-Output "* Adapting the dev config..."
 $ssh_initial = Get-Content -path ~\.ssh\config -Raw
 $ssh_new = $ssh_initial -replace "HostName fe80::ff:fe00:1%%.*", "HostName fe80::ff:fe00:1%%$idx"
 $ssh_new | Set-Content -NoNewline -Path ~\.ssh\config
-
-# Initialize the ssh key (docker would otherwise cause problems)
 ssh -o StrictHostKeyChecking=accept-new root@dev echo "ok"
 Write-Output "* Adapting the dev config done"
 
-Write-Output "* Launching PCloud..."
+Write-Output "* Waiting for P drive..."
 while (!(Test-Path "P:\")) { 
     Start-Sleep 5
 }
+Write-Output "* Waiting for P drive done"
+
+Write-Output "* Launching PCloud..."
 & "C:\Program Files\pCloud Drive\pCloud.exe"
 Write-Output "* Launching PCloud done"
 
