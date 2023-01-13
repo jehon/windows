17e546b909c97ea878a663c3b22fbee57cba3338
Jean Honlet <jehon@users.noreply.github.com>
2023-01-13 10:33:39 +0100

    feat: various updates

--- etc/jh-startup.ps1
+++ etc/jh-startup.ps1
@@ -18,20 +18,28 @@ $ssh_new | Set-Content -NoNewline -Path ~\.ssh\config
 ssh -o StrictHostKeyChecking=accept-new root@dev echo "ok"
 Write-Output "* Adapting the dev config done"
 
-Write-Output "* Launching Teams..."
+# Write-Output "* Launching Teams..."
 # https://superuser.com/a/1626176/287025
 # https://answers.microsoft.com/en-us/msteams/forum/all/teams-and-media-keys/136320bb-7af0-4bdc-8743-56608ff576b2?page=2
-& "C:\Users\jho\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe" --process-start-args ( "--profile=AAD", "-disable-features=HardwareMediaKeyHandling" )
-Write-Output "* Launching Teams done"
+# & "C:\Users\My Name\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe" --process-start-args "--disable-features=HardwareMediaKeyHandling --system-initiated"
+# cd C:\Users\jho\AppData\Local\Microsoft\Teams ???
+# & "C:\Users\jho\AppData\Local\Microsoft\Teams\Update.exe" --processStart "Teams.exe" --process-start-args ( "--profile=AAD", "-disable-features=HardwareMediaKeyHandling" )
+# Start-Process -File "C:\Users\jho\AppData\Local\Microsoft\Teams\current\Teams.exe" -ArgumentList --disable-features=HardwareMediaKeyHandling
+# Write-Output "* Launching Teams done"
 
-Write-Output "* Waiting for P drive..."
-while (!(Test-Path "P:\")) { 
-    Start-Sleep 5
+if (Test-Path "D:\") {
+	Write-Output "PCloud is already present at D:"
+} else {
+	Write-Output "* Waiting for P drive..."
+	while (!(Test-Path "P:\")) { 
+		Start-Sleep 5
+		Write-Output "."
+	}
+	Write-Output "* Waiting for P drive done"
+
+	Write-Output "* Launching PCloud..."
+	& "C:\Program Files\pCloud Drive\pCloud.exe"
+	Write-Output "* Launching PCloud done"
 }
-Write-Output "* Waiting for P drive done"
-
-Write-Output "* Launching PCloud..."
-& "C:\Program Files\pCloud Drive\pCloud.exe"
-Write-Output "* Launching PCloud done"
 
 pause
