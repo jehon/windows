99f62d4960cc144dcb552e49b1ce26b3b78aaef4
Jean Honlet <jehon@users.noreply.github.com>
2023-02-14 12:16:58 +0100

    refactor: move nodejs to devstack

--- bin/jh-with-timestamp.ps1
+++ bin/jh-with-timestamp.ps1
@@ -3,10 +3,14 @@ Param([String]$path)
 
 # Thanks to https://stackoverflow.com/a/1954384/1954789
 
-$dirName  = [io.path]::GetDirectoryName($path)
-if ($dirName) {
-    $dirName = $dirName + '\'
-}
+# $dirName  = [io.path]::GetDirectoryName($path)
+# if ($dirName) {
+#     $dirName = $dirName + '\'
+# }
+
+# Thanks to https://stackoverflow.com/a/31747246/1954789
+$dirName = [Environment]::GetFolderPath("Desktop")
+
 $filename = [io.path]::GetFileNameWithoutExtension($path)
 $ext      = [io.path]::GetExtension($path)
 $ts       = get-date -Format "yyyy-MM-dd hh-mm-ss"
